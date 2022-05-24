f7bfbb15275795c688400b11106aa46ea7a8c7fb
ChandanPk <chandanpk9@gmail.com>
2022-05-24 10:39:01 +0530

    update index.js

--- src/comps/Signup.js
+++ src/comps/Signup.js
@@ -1,18 +1,48 @@
 import { useRef, useState } from "react";
+import { useCookies } from "react-cookie";
 
 const Signup = () => {
 
-    const [emailErr, setEmailErr] = useState();
-    const [pwdErr, setPwdErr] = useState();
+    const [emailErr, setEmailErr] = useState(null);
+    const [pwdErr, setPwdErr] = useState(null);
+    const [cookies, setCookies] = useCookies();
 
     const emailRef = useRef()
     const pwdRef = useRef()
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
+        setEmailErr(null)
+        setPwdErr(null)
         e.preventDefault();
         const email = emailRef.current.value
         const password = pwdRef.current.value
-        console.log(email, password)
+
+        try {
+            const res = await fetch("http://localhost:8000/signup", {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+                headers: { 'Content-Type': 'application/json' }
+            })
+            const data = await res.json()
+            if (res.status === 201) {
+                setCookies('jwt', data.token, { path: '/' })
+                console.log(data)
+            } else {
+                throw data
+            }
+        } catch (error) {
+            // console.log("hillo")
+            // console.log(error)
+            if(error.errors) {
+                if(error.errors.email) {
+                    setEmailErr(error.errors.email)
+                }
+                if(error.errors.password) {
+                    setPwdErr(error.errors.password)
+                }
+            }
+        }
+
     }
 
 
@@ -20,9 +50,9 @@ const Signup = () => {
         <form onSubmit={(e) => handleSubmit(e)}>
             <h1>Signup</h1>
             <input type="email" ref={emailRef} placeholder="email" />
-            {emailErr && <div class="email-error error"></div>}
+            {emailErr && <div className="email-error error">{emailErr}</div>}
             <input type="password" ref={pwdRef} placeholder="password" />
-            {pwdErr && <div class="password-error error"></div>}
+            {pwdErr && <div className="password-error error">{pwdErr}</div>}
             <button>SUBMIT</button>
         </form>
     );
